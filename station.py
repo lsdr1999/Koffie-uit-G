7200cd47bfe3c6ed78a1adf2618233ff8321999d
Arf0r <maarten.wijstma@gmail.com>
2019-04-12 12:41:07 +0200

    station

--- station.py
+++ station.py
@@ -1,14 +1,22 @@
 class Station(object):
     """
-    Defines a station opbject, a station has a name, coordinates, and has
+    Defines a station object, a station has a name, coordinates, and has
     connections with other stations that take a certain time to reach
-
     """
 
-    def __init__(self, name, coordinates) :
-        super(, self).__init__()
+    def __init__(self, name, xcoordinate, ycoordinate, critical):
+        """
+        Initializes a station object
+        """
         self.name = name
-        self.coordinates = coordinates
-        self.connections = {}
-        self.visited = False
-        self.critical = False
+        self.xcoordinate = xcoordinate
+        self.ycoordinate = ycoordinate
+        self.connections = []
+        self.critical = critical
+
+    def addConnection(self, name, time, critical):
+        self.connections.append([name, time, critical])
+
+    def __str__(self):
+        return (f"{self.name}, {self.xcoordinate}, \
+{self.ycoordinate}, {self.critical}\n")
