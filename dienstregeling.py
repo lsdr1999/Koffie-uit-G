360882bb3e8a7b0fa19da47177f187d2b55ca5aa
Nienke1997 <nienke-l@live.nl>
2019-04-12 18:11:52 +0200

    dienstregeling naar nieuwe UML

--- dienstregeling.py
+++ dienstregeling.py
@@ -1,41 +1,35 @@
+from spoor import Spoor
+from traject import Trajectory
+
 class Dienstregeling():
 
-	def __init__(self, game):
-		self.trajecten = trajecten()
-		self.kritieke_stations = kritieke_stations ()
-		self.max_trajecten = 7 #voor vraag 1a
+	def __init__(self, spoor):
+		self.trajectories = 0 # how many trajectories are used
+		self.maxTrajectories = 7 # maximum of trajectories
+		self.qualityK = 0.0 # max quality K
+		self.totalCritical = 20 # sum of critical connections
+		self.visitedCriticalConnectionCount = 0 #count of connections visited
+		self.visitedConnections = [] #empty list of all of the visited connections
+		self.totalTime = 0 # total length in minutes of trajectories combined
 
-def load_stations(self, filename):
-	stations_data = []
-	with open(filename, "r") as f:
-		station_data = []
-		for line in f:
-			# when there is no blank newline it means there's still data.
-			if not line == "\n":
-				station_data.append(line.strip())
-			# a blank newline signals all data of a single room is parsed.
-			else:
-				stations_data.append(station_data)
-				station_data = []
-		# append a final time, because the files do not end on a blank newline.
-		stations_data.append(station_data)
+	def calculateScore(self):
+		"""
+		Calculates the quality of the lining
+		"""
+		p = float(self.visitedConnectionCount / self.totalCritical)
+		self.qualityK = p * 10000 - (self.trajectories * 20 + self.totalTime / 10)
 
-def move(self, direction):
-	"""
-	Moves to a different station in the specified direction.
-	"""
-	# conditional movement.
-	for directionx in self.current_room.route[direction.upper()]:
-		# check if condition includes item (like: id / item).
-		if directionx.isdigit() == False:
-			direction_split = directionx.split("/")
-			# check if item (found after splitting at "/"") is in inventory.
-			if direction_split[1] in self.inventory.inventory_list:
-				self.current_room = self.rooms[int(direction_split[0])]
-				break
-			else:
-				print("Item unavailable in inventory")
-		else:
-			# no item needed, just go to new room.
-			room_id = directionx
-			self.current_room = self.rooms[int(room_id)]
+	def addVisited(connection):
+		"""
+		Adds a visited station to the list of visited station and adds it to
+		the visitedConnectionCount if it is a critical connection
+		"""
+		if (station[critical] and not in self.visitedConnections):
+			self.visitedConnections.append(station)
+			self.visitedCriticalConnectionCount += 1
+		self.totalTime += int(station[time]) #even checken hoeveelste variable dit is van connection
+
+	def startTrajectory(self):
+		if (self.trajectories <= self.maxTrajectories):
+			#start traject ???
+			self.trajectories += 1
